4252c0c89f184df42b7fc87daf03e24302755572
haja <haja@student.chalmers.se>
2024-05-02 15:29:04 +0200

    Add the task description and the sloution for the Help the police exercise

--- Help The Police/main.py
+++ Help The Police/main.py
@@ -1,16 +1,90 @@
-# This is a sample Python script.
+import pickle
+import sys
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+people = 'Enter the path'
+numbers1= 'Enter the path'
+numbers2='Enter the path'
+numbers3='Enter the path'
+
+def main():
+    argument = sys.argv[1]
+    files = []
+    choice = 1
+    while choice != 2:
+        display_menu()
+        print()
+        choice = int(input('Enter choice: '))
+        if choice == 1:
+            file_path = input('Enter a filename (include full path): ')
+            try:
+                open(file_path)
+            except:
+                print('Error: There was a problem with at least one of the files.')
+                return
+            print()
+            files.append(file_path)
+
+        if choice == 2:
+            if not files:
+                print('Error: There was a problem with at least one of the files.')
+                return
+            final_set = cross_reference(files)
+            suspects = map_numbers_to_names(final_set, argument)
+            display_suspects(suspects)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def display_menu():
+    print('1. Add file')
+    print('2. Calculate')
+
+def cross_reference(files):
+    interaction_set = None
+    counter = 1
+    for file in files:
+        file_path = open(file, 'r')
+        set_name = f'number_set_{counter}'
+        locals()[set_name] = set()
+        current_set = locals()[set_name]
+        counter = counter + 1
+
+        for line in file_path:
+            current_set.add(line.strip())
+
+        file_path.close()
+
+        if interaction_set is None:
+            interaction_set = current_set
+        else:
+            interaction_set = interaction_set.intersection(current_set)
+    return interaction_set
+
+def map_numbers_to_names(numbers,filename):
+    names = []
+    input_file = open(filename, 'rb')
+    people_details = pickle.load(input_file)
+    input_file.close()
+    for number in numbers:
+        temporary = str(number)
+        found_user = False
+        for key, value in people_details.items():
+            if temporary == key:
+                names.append(value)
+                found_user = True
+                break
+        if not found_user:
+            unknown_user = 'Unknown (' + temporary + ')'
+            names.append(unknown_user)
+    return names
 
 
-# Press the green button in the gutter to run the script.
+def display_suspects(names):
+    print('The following persons was present on all crime scenes:')
+    print('------------------------------------------------------')
+    if not names:
+        print('No matches')
+    else:
+        for name in names:
+            print(name)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
